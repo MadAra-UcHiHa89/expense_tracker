ea96ff71bf52fc1c9945f24e4716571cf57ed029
Aadil Saudagar <aadilsaudagar24@gmail.com>
2023-09-29 13:20:52 +0530

    Created the model for ExpenseBucket

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -34,3 +34,24 @@ class Expense {
     return "${date.day}/${date.month}/${date.year}";
   }
 }
+
+class ExpenseBucket {
+  ExpenseBucket({required this.category, required this.expenses});
+
+  // Constuctor which filters out the expenses that don't belong to the category
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalAmount {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
+  }
+}
